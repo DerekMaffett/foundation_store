a9767f2a0d59107d3bd4772c8733f8ea31ed4f0d
Derek Maffett <derekgmaffett@gmail.com>
2014-09-19 17:45:07 -0700

    add stylings for products views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundationStore::Application.config.secret_token = 'aad4f3df28a5f995d236cafb1d86539ff55163c45d8c859bf4fc8eda86a470ab83d8dd77cd10d8f84627c7fcf7c518b9204afb062ef06a12c0c39d7a64b8fd2e'
